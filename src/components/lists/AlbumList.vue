988f001cd966c7c675b87cf5e6d80a8e2723ab36
Natalia Justicia <nataliajusticia@gmail.com>
2020-11-16 22:42:06 +0100

    Album component

--- src/components/lists/AlbumList.vue
+++ src/components/lists/AlbumList.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="tab-pane" id="canciones">
-    <h2>286 álbumes</h2>
+    <h2>{{ numberOfAlbums() }} álbumes</h2>
 
     <div class="row">
-      <div class="col-lg-2 col-md-4 album">
-        <img class="album__image" src="@/assets/albums/yellow-submarine.jpg" alt="Yellow Submarine Songtrack" loading="lazy">
-        <p class="album__title">Yellow Submarine Songtrack</p>
-        <p class="album__description">The Beatles</p>
-      </div>
+      <Album v-for="album in albums" v-bind:key="album.id" v-bind:title="album.title" v-bind:cover_xl="album.cover_xl" v-bind:artist="album.artist.name" />
     </div>
   </div>
 </template>
 
 <script>
+import Album from '@/components/Album'
+
 export default {
-  name: 'AlbumList'
+  name: 'AlbumList',
+  components: {
+    Album
+  },
+  props: {
+    albums: Array
+  },
+  methods: {
+    numberOfAlbums () {
+      return this.albums.length
+    }
+  }
 }
 </script>
-
-<style lang="scss">
-
-</style>
