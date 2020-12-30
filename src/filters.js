1a4beee852e4bbc1623ba65681a11f2eb05f37a4
Natalia Justicia <nataliajusticia@gmail.com>
2020-12-30 23:22:01 +0100

    Delete json folder

--- src/filters.js
+++ src/filters.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 
 // Format numbers
-Vue.filter('formatNumber', function (value) {
+Vue.filter('formatNumber', value => {
   const rounded = Math.round(value * 100) / 100
   return rounded.toString().replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.')
 })
 
 // Format time
-Vue.filter('formatTime', function (value) {
+Vue.filter('formatTime', value => {
   const hours = Math.floor(value / 60)
   const minutes = value % 60
   const hh = hours < 10 ? `0${hours}` : hours
