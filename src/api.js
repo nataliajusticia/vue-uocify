374ec22fe423db7720ec24c4a011b12459231cb7
Natalia Justicia <nataliajusticia@gmail.com>
2021-01-02 15:24:43 +0100

    Fetch data without heroku

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const url = 'https://cors-anywhere.herokuapp.com/'
 // Fetch playlists data
 export const getPlaylists = async () => {
   try {
-    const response = await fetch(`${url}https://api.deezer.com/chart/0/playlists`)
+    const response = await fetch('https://api.deezer.com/chart/0/playlists')
     return await response.json()
   } catch (err) {
     console.error(err)
